b3fa924d566c0b87e202a5993c2b6c5229b52995
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2022-04-14 18:32:05 +0200

    connect app with the API

--- script.js
+++ script.js
@@ -1,64 +1,99 @@
-// const api_data = require("./api_response.json");
-// console.log(api_data);
-const phonetic_template = (element) => {
+let inputval = document.querySelector(".js-input");
+let searchBtn = document.querySelector(".js-button");
+let phonetic = document.querySelector(".js-phonetics-container");
+let meaning = document.querySelector(".js-meaning-container");
+let APIUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => wordDetails(data));
+}
+
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  phonetic.classList.add("hidden");
+  meaning.classList.add("hidden");
+  fetchData(`${APIUrl}${inputval.value}`);
+});
+
+const phonetic_template = (data) => {
+  `<h1 class="js-header">${inputval.value}</h1>` + `<hr />`;
   let phoneticHTML =
     `<div class="phonetic js-phonetic">` +
-    `<a class="phonetic_link js-link" href="${element.audio}">Listen</a>` +
-    `<p class="phonetic_text js-text">${element.text}</p>` +
+    `<a class="phonetic_link js-link" href="${data.audio}"> <img class="phonetic_icon" src="sound.png" /> </a>` +
+    `<p class="phonetic_text js-text">${data.text}</p>` +
     `</div>`;
   return phoneticHTML;
 };
 
-const meaning_template = (element) => {
+const meaning_template = (data) => {
   let definitions_html = "";
-  for (definition of element.definitions) {
+  for (definition of data.definitions) {
     definitions_html += definition_template(definition);
   }
   let meaningHTML =
     `<div class="meaning js-meaning">` +
-    `<h1 class="partOfSpeech">${element.partOfSpeech}</h1>` +
+    `<h1 class="partOfSpeech">${data.partOfSpeech}</h1>` +
+    `<hr />` +
     definitions_html +
     `</div>` +
     `<div class="synonyms js-synonyms">`;
-  if (element.synonyms) {
+  if (data.synonyms) {
     meaningHTML +=
       `<strong>Similar:</strong>` +
       `<ul>` +
-      `<li>${element.synonyms}</li>` +
+      `<li>${data.synonyms}</li>` +
       `</ul>`;
   }
   meaningHTML += `</div>`;
   return meaningHTML;
 };
 
-const definition_template = (element) => {
-  let definitionHTML =
-    `<div class="js-definition>` + `<p>${element.definition}</p>`;
-  if (element.example) {
-    definitionHTML += `<em>${element.example}</em>`;
+const definition_template = (data) => {
+  let definitionHTML = `<div>` + `<p class="definition">${data.definition}</p>`;
+  if (data.example) {
+    definitionHTML += `<span class="quote">${data.example}</span>`;
   }
   definitionHTML +=
-    `<p>${element.synonyms}</p>` + `<p>${element.antonyms}</p>` + `</div>`;
+    `<p>${data.synonyms}</p>` + `<p>${data.antonyms}</p>` + `</div>`;
   return definitionHTML;
 };
 
-fetch("./api_response.json")
-  .then((response) => response.json())
-  .then((json) => {
-    const html_phonetics_section = document.querySelector(
-      ".js-phonetics-container"
-    );
-    const html_meanings_section = document.querySelector(
-      ".js-meaning-container"
-    );
-    console.log(html_meanings_section);
-    for (record of json) {
-      for (phonetic of record.phonetics) {
-        html_phonetics_section.innerHTML =
-          html_phonetics_section.innerHTML + phonetic_template(phonetic);
-      }
-      for (meaning of record.meanings) {
-        html_meanings_section.innerHTML += meaning_template(meaning);
-      }
+function wordDetails(data) {
+  phonetic.classList.remove("hidden");
+  meaning.classList.remove("hidden");
+  const html_phonetics_section = document.querySelector(
+    ".js-phonetics-container"
+  );
+  const html_meanings_section = document.querySelector(".js-meaning-container");
+  // console.log(html_meanings_section);
+  for (record of data) {
+    for (phonetic of record.phonetics) {
+      html_phonetics_section.innerHTML += phonetic_template(phonetic);
     }
-  });
+    for (meaning of record.meanings) {
+      html_meanings_section.innerHTML += meaning_template(meaning);
+    }
+  }
+}
+// fetch("./api_response.json")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     const html_phonetics_section = document.querySelector(
+//       ".js-phonetics-container"
+//     );
+//     const html_meanings_section = document.querySelector(
+//       ".js-meaning-container"
+//     );
+//     console.log(html_meanings_section);
+//     for (record of json) {
+//       for (phonetic of record.phonetics) {
+//         html_phonetics_section.innerHTML =
+//           html_phonetics_section.innerHTML + phonetic_template(phonetic);
+//       }
+//       for (meaning of record.meanings) {
+//         html_meanings_section.innerHTML += meaning_template(meaning);
+//       }
+//     }
+//   });
